f8d6d46dc2d55777452b9fab8d1c378320f7c406
Rehnuma Reza <rehnuma.reza11@gmail.com>
2023-11-26 20:03:17 +0600

    Initial Commit

---
+++ LiveTextInteractionDemo/LiveTextInteractionDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  LiveTextInteractionDemoApp.swift
+//  LiveTextInteractionDemo
+//
+//  Created by Rehnuma Reza on 26/11/23.
+//
+
+import SwiftUI
+
+@main
+struct LiveTextInteractionDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
